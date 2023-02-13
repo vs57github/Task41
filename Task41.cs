15ad29b58123357f968a1e55225691bd13ec4b11
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-13 20:43:30 +0300

    Add files via upload

---
+++ Task41.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел: ");
+
+            int M = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < M; i++) 
+            { 
+                Console.Write("Введите число: ");
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > 0) 
+                    count++; 
+            }
+
+            Console.WriteLine($"Количество положительных чисел: {count}");
+
+            Console.ReadKey();
+        }  
+    }  
+}
